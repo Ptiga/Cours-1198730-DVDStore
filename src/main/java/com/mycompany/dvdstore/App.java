009ef9cf286bcc0d31e2ed889197bc32f777f9ce
Ptiga <tylerdurdenxp@gmail.com>
2022-09-07 17:20:04 +0200

    Exercice n°11

--- src/main/java/com/mycompany/dvdstore/App.java
+++ src/main/java/com/mycompany/dvdstore/App.java
@@ -1,15 +1,8 @@
 package com.mycompany.dvdstore;
 
-import com.mycompany.dvdstore.controller.MovieController;
 import com.mycompany.dvdstore.controller.MovieControllerInterface;
-import com.mycompany.dvdstore.repository.FileMovieRepository;
-import com.mycompany.dvdstore.repository.MovieRepositoryInterface;
-import com.mycompany.dvdstore.service.DefaultMovieService;
-import com.mycompany.dvdstore.service.MovieServiceInterface;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import java.util.Scanner;
 
 
 public class App 
