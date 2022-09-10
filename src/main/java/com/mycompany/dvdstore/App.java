7d7ebad32e455eebe5d478a529eb0b936ddacebb
Ptiga <tylerdurdenxp@gmail.com>
2022-09-10 14:14:46 +0200

    Exercice n°12 - correction

--- src/main/java/com/mycompany/dvdstore/App.java
+++ src/main/java/com/mycompany/dvdstore/App.java
@@ -2,6 +2,7 @@ package com.mycompany.dvdstore;
 
 import com.mycompany.dvdstore.controller.MovieControllerInterface;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
@@ -10,7 +11,7 @@ public class App
     public static void main( String[] args )
     {
 
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
         MovieControllerInterface movieController = applicationContext.getBean(MovieControllerInterface.class);
 
         movieController.addUsingConsole();
