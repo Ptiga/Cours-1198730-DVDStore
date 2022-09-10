9b3da180b494b4faab7f3a924df0f68c36cd0f80
Ptiga <tylerdurdenxp@gmail.com>
2022-09-10 17:07:29 +0200

    Exercice n°14

--- src/main/java/com/mycompany/dvdstore/App.java
+++ src/main/java/com/mycompany/dvdstore/App.java
@@ -5,28 +5,25 @@ import com.mycompany.dvdstore.repository.MovieRepositoryInterface;
 import com.mycompany.dvdstore.repository.file.FileMovieRepository;
 import com.mycompany.dvdstore.repository.memory.MemoryMovieRepository;
 import com.mycompany.dvdstore.service.MovieServiceInterface;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.*;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
-@Configuration
-@ComponentScan(basePackages = {"com.mycompany.dvdstore.controller", "com.mycompany.dvdstore.service"/*, "com.mycompany.dvdstore.repository.file"*/})
-@PropertySource("classpath:application.properties")
+@SpringBootApplication
 public class App 
 {
     public static void main( String[] args )
     {
 
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(App.class);
+        ApplicationContext applicationContext = SpringApplication.run(App.class);
         MovieControllerInterface movieController = applicationContext.getBean(MovieControllerInterface.class);
 
         movieController.addUsingConsole();
     }
 
-    @Bean
-    public MovieRepositoryInterface configureMovieRepository(){
-        return new FileMovieRepository();
-    }
+
 
 }
